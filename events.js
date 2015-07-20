3e11b9cacda32ca6368204b579858565e7ccf854
Erik Dyer <erik.dyer@lampcamp.net>
2015-07-20 21:45:08 +0000

    Created new file to hold events, began foundations

---
+++ events.js
@@ -0,0 +1,11 @@
+/* All events in ButtonClicker
+There are three types of events:
+  Story events:
+    Events that lead to a progression in the story line
+  Action events:
+    Events that lead to further possible Action
+  Passive events:
+    Events that have no dynamic functionality attached.
+*/
+
+Math.floor(Math.random() * 6) + 1;  
